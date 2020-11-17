dd12345fc4833748b6992d586f15252c76b52138
63070052 <73658351+63070052@users.noreply.github.com>
2020-11-17 11:10:01 +0700

    Update and rename update.php to edit.php

---
+++ edit.php
@@ -0,0 +1,23 @@
+<?php
+
+$conn = mysqli_init();
+mysqli_real_connect($conn, 'itflab52.mysql.database.azure.com', 'it63070052@itflab52', 'YHVexv86', 'ITFlab', 3306);
+if (mysqli_connect_errno($conn))
+{
+    die('Failed to connect to MySQL: '.mysqli_connect_error());
+}
+
+$name = $_POST['name'];
+$comment = $_POST['comment'];
+
+$sql = "UPDATE guestbook SET Comment='$comment' WHERE Name='$name' ";
+
+if (mysqli_query($conn, $sql)) {
+    echo "New record created successfully! CONGRATULATION!";
+  } else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+  }
+  
+mysqli_close($conn);
+?>
+
