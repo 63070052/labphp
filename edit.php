a4144bba7caa27261650afef77f6d2df03cbe348
63070052 <73658351+63070052@users.noreply.github.com>
2020-11-19 17:16:21 +0700

    Update edit.php

--- edit.php
+++ edit.php
@@ -13,7 +13,7 @@ $comment = $_POST['comment'];
 $sql = "UPDATE guestbook SET Comment='$comment' WHERE Name='$name' ";
 
 if (mysqli_query($conn, $sql)) {
-    header('Location: https://phpscript52.azurewebsites.net/show.php');
+    echo "New record created successfully! CONGRATULATION!";
 
   } else {
     echo "Error: " . $sql . "<br>" . mysqli_error($conn);
